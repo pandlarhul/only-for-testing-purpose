d3c07eac713edddf4cccc657adc978dc8b5669c4
Rahul Rohilla <rahul.kumar13@mountblue.tech>
2020-08-14 19:44:09 +0530

    removed all data

--- src/server/config.js
+++ src/server/config.js
@@ -1,17 +1,17 @@
-const dotenv = require("dotenv");
-try{
-  let result = dotenv.config();
-  if(result.error){
-    throw result.error;
-  }
-  const {parsed:envs} = result;
-  envs.PORT= envs.PORT || "3306"
-  envs.HOST_NAME= envs.HOST_NAME || "us-cdbr-east-02.cleardb.com"
-  envs.USER=envs.USER || "b258267de28220"
-  envs.PASSWORD=envs.PASSWORD || "850330cb"
-  envs.DATABASE_NAME=envs.DATABASE_NAME || "heroku_9936182822b94a5"
+// const dotenv = require("dotenv");
+// try{
+//   let result = dotenv.config();
+//   if(result.error){
+//     throw result.error;
+//   }
+//   const {parsed:envs} = result;
+//   envs.PORT= envs.PORT || "3306"
+//   envs.HOST_NAME= envs.HOST_NAME || "us-cdbr-east-02.cleardb.com"
+//   envs.USER=envs.USER || "b258267de28220"
+//   envs.PASSWORD=envs.PASSWORD || "850330cb"
+//   envs.DATABASE_NAME=envs.DATABASE_NAME || "heroku_9936182822b94a5"
 
-  module.exports = envs;
-}catch(error){
-  console.log(error.message);
-}
+//   module.exports = envs;
+// }catch(error){
+//   console.log(error.message);
+// }
